1124cb2aa1fa2688b42b3b9b71dc7bd6073f3ad3
Akbaralishaikh-devop <137177751+Akbaralishaikh-devop@users.noreply.github.com>
2023-08-18 18:38:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo /home/ec2-user/tomcat/bin/shutdown.sh
-aws s3 cp s3://cicdbucketformine/demo-build/LoginWebApp-1.war /tmp
+aws s3 cp s3://cicdbucketformine/javaproject/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /home/ec2-user/tomcat/webapps/
 sudo /home/ec2-user/tomcat/bin/startup.sh
